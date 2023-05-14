7dd918456e3787c3a73ebc0632ff8549bc11fe77
MrKolev <100051403+MrKolev@users.noreply.github.com>
2023-05-14 18:56:45 +0300

    add Basic Validation & Providing Feedback

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,10 +1,42 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+  const [nameInputIsValid, setNameInputIsValid] = useState(true)
+
+  const nameInputChangedHandler = (e) => {
+    setEnteredName(e.target.value)
+  }
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+
+    if (enteredName.trim() == "") {
+      setNameInputIsValid(false);
+      return
+    }
+
+    console.log(enteredName);
+    const enterValue = nameInputRef.current.value;
+    console.log(`ref: ${enterValue}`);
+
+    setEnteredName("")
+    setNameInputIsValid(true);
+  }
+
   return (
-    <form>
-      <div className='form-control'>
+    <form onSubmit={formSubmitHandler}>
+      <div className={`form-control ${!nameInputIsValid && "invalid"}`}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input
+          ref={nameInputRef}
+          type='text'
+          value={enteredName}
+          id='name'
+          onChange={nameInputChangedHandler} />
       </div>
+      {!nameInputIsValid && <p className="error-text">Please enter your name.</p>}
       <div className="form-actions">
         <button>Submit</button>
       </div>
